0f6f2b16bd8fca06b36736affdb9ed095504cf48
Sekou Cox <34495242+sekoucox7913@users.noreply.github.com>
2022-11-15 04:15:37 -0500

    Add files via upload

---
+++ NYCHSDetailTableViewController.swift
@@ -0,0 +1,62 @@
+//
+//  NYCHSDetailTableViewController.swift
+//  20180817-AfnanMirza-NYCSchools
+//
+//  Created by Afnan Mirza on 8/17/18.
+//  Copyright © 2018 Afnan Mirza. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import MapKit
+
+
+class NYCHSDetailTableViewController: UITableViewController {
+    
+    var HSWithSatScore: NYCHighSchools!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.title = HSWithSatScore.schoolName
+        self.tableView.rowHeight = UITableViewAutomaticDimension
+        
+    }
+
+}
+
+extension NYCHSDetailTableViewController {
+    // MARK: - Table view data source
+    
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        CellAnimator.animate(cell, withDuration: 0.6, animation: CellAnimator.AnimationType(rawValue: 5)!)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4 
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.row {
+        case 0:
+            return DetailCells.tableViewCellWithSATScore(self.tableView, HSWithSatScore: self.HSWithSatScore)
+        case 1:
+            return DetailCells.tableViewCellWithOverView(self.tableView, HSWithSatScore: self.HSWithSatScore)
+        case 2:
+            return DetailCells.tableViewCellWithContactInfo(self.tableView, HSWithSatScore: self.HSWithSatScore)
+        default:
+            return DetailCells.tableViewCellWithAddress(self.tableView, HSWithSatScore: self.HSWithSatScore)
+        }
+    }
+    
+    //MARK: - UITable View Delegate
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath.row {
+        case 0,1,2:
+            return UITableViewAutomaticDimension
+        default:
+            return UIScreen.main.bounds.width * 0.7
+        }
+    }
+    
+}
